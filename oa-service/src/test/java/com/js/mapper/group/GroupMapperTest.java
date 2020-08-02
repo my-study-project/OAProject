7f2cda14c2f790024a4c0e1391aaa16ca08935ae
jiangshuang <txtAPOTOX4869>
2020-08-02 19:29:48 +0800

    fix-bug

--- oa-service/src/test/java/com/js/mapper/group/GroupMapperTest.java
+++ oa-service/src/test/java/com/js/mapper/group/GroupMapperTest.java
@@ -1,10 +1,15 @@
 package com.js.mapper.group;
 
 import com.js.BaseTest;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class GroupMapperTest extends BaseTest {
+public class GroupMapperTest extends BaseTest {
 
+    @Test
+    public void test() {
+        System.out.println("test1");
+    }
 }
