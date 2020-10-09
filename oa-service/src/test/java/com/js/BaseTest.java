6a3aac0fe535306fe0053863c072e6d4109cc3df
姜爽 <2545251075@qq.com>
2020-10-09 13:46:51 +0800

    fix - jar

--- oa-service/src/test/java/com/js/BaseTest.java
+++ oa-service/src/test/java/com/js/BaseTest.java
@@ -1,10 +1,10 @@
 package com.js;
 
 import junit.framework.TestCase;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @program: BaseTest
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @Author: jiangshuang
  * @Description: 编写测试类公共父类
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class BaseTest extends TestCase {
