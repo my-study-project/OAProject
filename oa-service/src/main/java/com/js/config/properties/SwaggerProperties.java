9971ef38bbc50fca872704df0a747bc7e1b5fbb1
姜爽 <2545251075@qq.com>
2020-10-10 09:19:54 +0800

    fix--swagger

---
+++ oa-service/src/main/java/com/js/config/properties/SwaggerProperties.java
@@ -0,0 +1,43 @@
+package com.js.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: OAProject
+ * @Date: 2020/10/10 8:58
+ * @Author: jiangshuang
+ * @Description:
+ */
+
+@Component
+@ConfigurationProperties("swagger")
+@Data
+public class SwaggerProperties {
+    /**
+     * 是否开启swagger，生产环境一般关闭，所以这里定义一个变量
+     */
+    private Boolean enable;
+
+    /**
+     * 项目应用名
+     */
+    private String applicationName;
+
+    /**
+     * 项目版本信息
+     */
+    private String applicationVersion;
+
+    /**
+     * 项目描述信息
+     */
+    private String applicationDescription;
+
+    /**
+     * 接口调试地址
+     */
+    private String tryHost;
+
+}
